29a5618d796c270054ed81cf2903a0b0ba039f12
adiel castano <adiel castano>
2017-03-08 04:17:30 -0800

    first commit

---
+++ hpc1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using MPI;
+
+namespace hpc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (new MPI.Environment(ref args))
+            {
+                // MPI program goes here!
+                Console.WriteLine("Hello, World! from rank " + Communicator.world.Rank
+                  + " (running on " + MPI.Environment.ProcessorName + ")");
+            }
+        }
+    }
+}
